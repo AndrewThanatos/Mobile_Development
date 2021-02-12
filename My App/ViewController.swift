516a684f3b4f5010843c53655e9059857312e501
a.berezhniuk <a.berezhniuk@booongo.com>
2021-02-12 19:25:41 +0200

    change background color and add segmented control

--- My App/ViewController.swift
+++ My App/ViewController.swift
@@ -13,7 +13,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
+class DrawingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    @IBAction func SegmentedControl(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("1")
+        case 1:
+            print("2")
+        default:
+            break
+        }
+    }
+}
