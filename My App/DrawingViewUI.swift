98fdaa82b2ce8fe2d9713ab2262ee804d663fa23
a.berezhniuk <a.berezhniuk@booongo.com>
2021-02-14 23:17:38 +0200

    rect orientation problem

--- My App/DrawingViewUI.swift
+++ My App/DrawingViewUI.swift
@@ -8,13 +8,56 @@
 import UIKit
 
 class DrawingViewUI: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+     
+        self.backgroundColor = UIColor.white
     }
-    */
+     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.backgroundColor = UIColor.white
+    }
+    
+    private func setup() {
+        backgroundColor = .clear
+    }
+
+    var path: UIBezierPath!
+
+    func createRectangle() {
+        // Initialize the path.
+        path = UIBezierPath()
+     
+        // Specify the point that the path should start get drawn.
+        path.move(to: CGPoint(x: 0.0, y: 0.0))
+     
+        // Create a line between the starting point and the bottom-left side of the view.
+        path.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height / 2))
+     
+        // Create the bottom line (bottom-left to bottom-right).
+        path.addLine(to: CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2))
+     
+        // Create the vertical line from the bottom-right to the top-right side.
+        path.addLine(to: CGPoint(x: self.frame.size.width / 2, y: 0.0))
+     
+        // Close the path. This will create the last line automatically.
+        path.close()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        self.createRectangle()
+     
+        // Specify the fill color and apply it to the path.
+        UIColor.orange.setFill()
+        path.fill()
+     
+        // Specify a border (stroke) color.
+        UIColor.purple.setStroke()
+        path.stroke()
+    }
+    
 
 }
