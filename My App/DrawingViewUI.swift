0d3bb524a2db64c4efef1363147c87a0f8e47b46
a.berezhniuk <a.berezhniuk@booongo.com>
2021-02-14 12:45:51 +0200

    rect orientation problem

---
+++ My App/DrawingViewUI.swift
@@ -0,0 +1,20 @@
+//
+//  DrawingViewUI.swift
+//  My App
+//
+//  Created by a.berezhniuk on 13.02.2021.
+//
+
+import UIKit
+
+class DrawingViewUI: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
