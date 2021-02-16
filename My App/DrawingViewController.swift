7e0efdba728567e83b4378ba51109eea4fa32277
a.berezhniuk <a.berezhniuk@booongo.com>
2021-02-17 00:36:44 +0200

    make segmantedControl

--- My App/DrawingViewController.swift
+++ My App/DrawingViewController.swift
@@ -10,13 +10,41 @@ import UIKit
 class DrawingViewController: UIViewController {
     
     private var chartView: ChartView!
+    private var graphView: GraphView!
     
+    @IBOutlet var mainView: ChartView!
+    
+    @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            chartView = ChartView()
+            mainView = chartView
+            mainView.backgroundColor = .red
 
+        case 1:
+            graphView = GraphView()
+        default:
+            break
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        chartView = ChartView()
-     
-//        self.view.addSubview(chartView)
+
+        
+//        switch segmentControl.selectedSegmentIndex {
+//        case 0:
+//            chartView = ChartView()
+//            print("Chart")
+//        case 1:
+//            graphView = GraphView()
+//            print("Graph")
+//        default:
+//            break
+//        }
+        
+        
     }
 
 }
